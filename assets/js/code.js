8f747ff940ebc4670ea5cd059116ab1a070c4b28
Greg D <gregorydesmarais@gmail.com>
2019-08-22 11:04:23 -0400

    Added code to get info from Giphy API and add it to images, then append images to 'gifDisplay' div with Rating.

--- assets/js/code.js
+++ assets/js/code.js
@@ -4,7 +4,7 @@ function displayButtons() {
     $("#searchButtons").empty();
     for (x in topics) {
         var newButton = $("<button>");
-        newButton.addClass("animals")
+        newButton.addClass("topic")
             .attr("data-info", topics[x])
             .text(topics[x]);
         $("#searchButtons").append(newButton);
@@ -12,7 +12,36 @@ function displayButtons() {
     }
 }
 
+$(document).on("click", ".topic", function() {
+    var search = $(this).attr("data-info");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=XQiqB5T5zXzeN0k6dUAvPr4MV7P2U2hE&q=" + search + "&limit=10";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        var data = response.data;
+        var static = "";
+        var animate = "";
+        for (x in data) {
+            var newDiv = $("<div>");
+            newDiv.addClass("left");
+            var newImg = $("<img>");
+            newImg.attr("data-static", data[x].images.fixed_height_still.url)
+                .attr("data-animate", data[x].images.fixed_height.url)
+                .attr("src", data[x].images.fixed_height_still.url)
+                .attr("state", "still")
+                .addClass("gif")
+                .appendTo(newDiv);
+            newDiv.append("Rating: " + data[x].rating)
+                .prependTo($("#gifDisplay"));
+        }
+    })
+})
+
+
+
 $(function() {
 
     displayButtons();
+
 })
