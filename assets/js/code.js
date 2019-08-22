1639c884a056b9e2517464fef8c2adcfb59df38e
Greg D <gregorydesmarais@gmail.com>
2019-08-22 11:16:15 -0400

    completed code to start and stop gif animation

--- assets/js/code.js
+++ assets/js/code.js
@@ -28,16 +28,30 @@ $(document).on("click", ".topic", function() {
             var newImg = $("<img>");
             newImg.attr("data-static", data[x].images.fixed_height_still.url)
                 .attr("data-animate", data[x].images.fixed_height.url)
+                .attr("data-state", "static")
                 .attr("src", data[x].images.fixed_height_still.url)
-                .attr("state", "still")
                 .addClass("gif")
                 .appendTo(newDiv);
             newDiv.append("Rating: " + data[x].rating)
                 .prependTo($("#gifDisplay"));
         }
     })
-})
+});
 
+$(document).on("click", ".gif", function() {
+    var state = $(this).attr("data-state");
+    var static = $(this).attr("data-static");
+    var animate = $(this).attr("data-animate");
+    console.log(state, static, animate);
+
+    if (state === "static") {
+        $(this).attr("data-state", "animate");
+        $(this).attr("src", animate);
+    } else {
+        $(this).attr("data-state", "static");
+        $(this).attr("src", static);
+    }
+});
 
 
 $(function() {
